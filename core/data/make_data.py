09c31e28d797223deb902f62321f7d5f9bfe10ca
stasstaf <stasstaf2002@gmail.com>
2024-05-17 00:10:18 +0300

    Add DDP support for ESM

--- core/data/make_data.py
+++ core/data/make_data.py
@@ -12,24 +12,34 @@ rng = np.random.default_rng(SEED)
 
 
 class EsmDataset(Dataset):
-    def __init__(self, file_path, tokenizer, max_length=254):
-        self.sequences = [str(record.seq) for record in SeqIO.parse(file_path, "fasta")]
-        self.tokenizer = tokenizer
-        self.max_length = max_length
+    def __init__(self, file_path, tokenizer, max_length=254, chunk_size=254):
+        sequences = [str(record.seq) for record in SeqIO.parse(file_path, "fasta")]
+        pad_token = tokenizer.eos_token
+        all_text = pad_token.join(sequences)
+
+        self.encoding = tokenizer(
+            all_text,
+            truncation=False,
+            max_length=None,
+            padding=False,
+            add_special_tokens=False
+        )
+
+        self.chunk_size = chunk_size
+        self.num_chunks = len(self.encoding['input_ids']) // self.chunk_size
 
     def __len__(self):
-        return len(self.sequences)
+        return self.num_chunks
 
     def __getitem__(self, idx):
-        sequence = self.sequences[idx]
-        encoding = self.tokenizer(
-            sequence,
-            truncation=True,
-            max_length=self.max_length,
-            padding='max_length',
-            return_attention_mask=False
-        )
-        return encoding
+        start_index = idx * self.chunk_size
+        end_index = start_index + self.chunk_size
+
+        chunked_input_ids = self.encoding['input_ids'][start_index:end_index]
+        attention_mask = [1] * len(chunked_input_ids)
+
+        return {"input_ids": torch.tensor(chunked_input_ids, dtype=torch.long),
+                "attention_mask": torch.tensor(attention_mask, dtype=torch.long)}
 
 
 def make_esm_dataset(file_path, tokenizer):
